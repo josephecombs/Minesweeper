6bb218e92edc57896aedff827a0d5ca612ebbf11
App Academy Student <student@appacademy.io>
2014-08-11 18:32:04 -0700

    Finished minsweeper project with YAML saving/loading of game state

--- minesweeper.rb
+++ minesweeper.rb
@@ -1,3 +1,6 @@
+require 'json'
+require 'yaml'
+
 class Game
   
   def initialize(num_mines, dim)
@@ -11,8 +14,6 @@ class Game
     until over
       puts "Board state:"
       @board.get_masked_state
-      p @board
-      gets.chomp
       args = get_input
       over = process_input(args)
       
@@ -22,6 +23,8 @@ class Game
   
   #player gives input, return true/false if mine has been revealed
   def get_input 
+    puts "to save this game, input 's,filename'"
+    puts "to load a game, input 'l,filename'"
     puts "input a coordinate to access. prefix with r for reveal or f for flag"
     puts "example 'f,1,2' places a flag at 1,2"
     input = gets.chomp
@@ -30,20 +33,24 @@ class Game
   end
   
   def process_input(args)
-    if args[0] == 'r'
+    if args[0] == "f" || args[0] == "r"
       chosen_tile = @board.board[Integer(args[1])][Integer(args[2])]
-      
+    end
+    if args[0] == 'r'
       if chosen_tile.has_mine
         puts "You clicked on a mine!"
         return true
       end
-      
-      
-      @board.board = chosen_tile.reveal(@board.board, @board.dim)
-      #should return coordinates of tiles that are now revealed
-      
+      chosen_tile.reveal(@board.board)
     elsif args[0] == 'f'
-      #flag the coordinate
+      chosen_tile.flagged = true
+    elsif args[0] == 's'
+      save_state = @board.to_yaml
+      f = File.open(args[1], 'w')
+      f.write(save_state)
+    elsif args[0] == 'l'
+      @board = YAML::load(File.read(args[1]))
+      
     else
       puts "incorrect input"
     end
@@ -62,7 +69,6 @@ class Board
     @board = create_random_board(num_mines)
     bestow_coordinates
     paint_all_neighbors
-    bestow_neighbors
   end
 
   def create_random_board(num_mines)
@@ -81,17 +87,35 @@ class Board
     @dim.times do |i|
       @dim.times do |j|
         @board[i][j].coordinates = [i, j]
+        @board[i][j].neighbors = bestow_neighbors(i, j, @dim)        
       end
     end
   end
   
-  def bestow_neighbors
-    @dim.times do |i|
-      @dim.times do |j|
-        @board[i][j].set_neighbors(@board, @dim)
-      end
+  def bestow_neighbors(i, j, dim)
+    neighbor_coordinates = NEIGHBOR_OFFSETS.map do |coord|
+      [i + coord[0], j + coord[1]]
     end
+    
+    neighbor_coordinates.map! do |coord2|
+      (0...dim).cover?(coord2[0]) && (0...dim).cover?(coord2[1]) ? coord2 : nil
+    end
+
+    neighbor_coordinates.compact
   end
+  
+  # def set_neighbors(board, dim)
+#     #iterate through all neighbors on the board and put them in an array
+#     neighbor_coordinates = NEIGHBOR_OFFSETS.map do |coord|
+#       [@coordinates[0] + coord[0], @coordinates[1] + coord[1]]
+#     end
+#
+#     neighbor_coordinates.each do |coord2|
+#       coord2 = nil unless (0...dim).cover?(coord2[0]) && (0...dim).cover?(coord2[1])
+#     end
+#
+#     neighbor_coordinates.compact
+#   end
   
   NEIGHBOR_OFFSETS = [
     [-1, -1],
@@ -136,12 +160,19 @@ class Board
   end
   
   def get_masked_state
+    true_print_grid = []
+    
     grid = Array.new(@dim) { Array.new(@dim, "_") }
     @board.each_with_index do |row, i|
       row.each_with_index do |tile, j|
         grid[i][j] = tile.display_masked_state
       end
-      puts grid[i].join
+      true_print_grid << grid[i].join
+    end
+    true_print_grid.reverse!
+    
+    true_print_grid.each do |line|
+      puts line
     end
   end
   
@@ -155,7 +186,7 @@ end
 
 class Tile
   attr_reader :revealed, :has_mine
-  attr_accessor :neighbor_mine_count, :flagged, :coordinates
+  attr_accessor :neighbor_mine_count, :flagged, :coordinates, :neighbors
   
   def initialize(has_mine)
     @has_mine = has_mine
@@ -170,62 +201,42 @@ class Tile
     @has_mine = bool
   end
   
-  def reveal(board, dim)
+  def reveal(board)
+    return if @revealed
     @revealed = true
     #if a mine, return
-    local_board = board.dup
-    #if @neighbor_mine_count == 0
+    
+    
     # iterate through neighbors
     #  if there is a mine or a flag, don't reveal. else reveal
     # 
-    #neighbor_coordinates = neighbors(local_board, dim)
-    @neighbors.each do |neighbor|
-      cur_neighbor_tile = local_board[neighbor[0]][neighbor[1]]
-      if !(cur_neighbor_tile.has_mine || cur_neighbor_tile.flagged)
-        cur_neighbor_tile.reveal(local_board, dim)
-      end
+    #neighbor_coordinates = neighbors(board, dim)
+    
+    if @neighbor_mine_count > 0 || @has_mine
+      return
     end
     
-    local_board
+    # if (cur_neighbor_tile.has_mine || cur_neighbor_tile.flagged)
+#       return board
+#     else
+    p @neighbors
+    @neighbors.each do |neighbor|
+      cur_neighbor_tile = board[neighbor[0]][neighbor[1]]
+      cur_neighbor_tile.reveal(board)
+    end
+
   end
   
   def display_masked_state
-
+    return "F" if @flagged == true
     return self.neighbor_mine_count if @revealed
+    
     "_"
-    
-
   end
-  
-  NEIGHBOR_OFFSETS = [
-    [-1, -1],
-    [ 0, -1],
-    [ 1, -1],
-    [ 1,  0],
-    [ 1,  1],
-    [ 0,  1],
-    [-1,  1],
-    [-1,  0]
-  ]
-
-  def set_neighbors(board, dim)
-    #iterate through all neighbors on the board and put them in an array
-    neighbor_coordinates = NEIGHBOR_OFFSETS.map do |coord|
-      [@coordinates[0] + coord[0], @coordinates[1] + coord[1]]
-    end
-    
-    neighbor_coordinates.each do |coord2|
-      coord2 = nil unless (0...dim).cover?(coord2[0]) && (0...dim).cover?(coord2[1])
-    end
-    
-    neighbor_coordinates.compact
-  end
-  
-  #create a mine count by iterating through the neighbors array
   
 end
 
-a = Game.new(8, 4)
+a = Game.new(1, 4)
 a.play_game
 
 # b = Board.new(5, 4)
